e922d36ff1abc97ecb4da1b1cce1c12264229120
VivienIngrams <vivingrams@gmail.com>
2023-05-23 17:12:03 +0100

    replaceCart reducer added

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -1,31 +1,39 @@
-import { uiActions } from "./ui-slice";
-import { cartActions } from "./cart-slice";
+import { uiActions } from './ui-slice';
+import { cartActions } from './cart-slice';
 
 export const fetchCartData = () => {
   return async (dispatch) => {
     const fetchData = async () => {
-      const response = await fetch('https://react-tasks-a94be-default-rtdb.europe-west1.firebasedatabase.app/cart.json');
-      
-      if (!response.ok) {
-        throw new Error('Could not fetch cart data!')
-        };
-        const data = await response.json();
+      const response = await fetch(
+        'https://products-426f6-default-rtdb.europe-west1.firebasedatabase.app/cart.json'
+      );
 
-        return data;
-      };
-      try {
-        const cartData = await fetchData();
-        dispatch(cartActions.replaceCart(cartData));
-      } catch (error) {
-        dispatch(
-            uiActions.showNotification({
-              status: "error",
-              title: "Error!",
-              message: "Failed to fetch cart data!",
-            })
-          );
+      if (!response.ok) {
+        throw new Error('Could not fetch cart data!');
       }
-  
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Fetching cart data failed!',
+        })
+      );
+    }
   };
 };
 
@@ -33,20 +41,26 @@ export const sendCartData = (cart) => {
   return async (dispatch) => {
     dispatch(
       uiActions.showNotification({
-        status: "pending",
-        title: "Sending ...",
-        message: "Sending cart data",
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!',
       })
     );
 
     const sendRequest = async () => {
       const response = await fetch(
-        "https://react-tasks-a94be-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
+        'https://products-426f6-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
       );
 
       if (!response.ok) {
-        throw new Error("Failed to send cart data");
+        throw new Error('Sending cart data failed.');
       }
     };
 
@@ -55,17 +69,17 @@ export const sendCartData = (cart) => {
 
       dispatch(
         uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Successfully sent cart data",
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
         })
       );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Failed to send cart data!",
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
         })
       );
     }
